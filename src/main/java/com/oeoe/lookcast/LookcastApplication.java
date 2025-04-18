2011746c5488fa6cc017083dd2c2a0287b3bc695
harper <35358294+yooyouny@users.noreply.github.com>
2025-04-19 00:41:23 +0900

    Initial commit

---
+++ src/main/java/com/oeoe/lookcast/LookcastApplication.java
@@ -0,0 +1,13 @@
+package com.oeoe.lookcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookcastApplication.class, args);
+	}
+
+}
